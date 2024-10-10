38caa1c68fd83d38328240d86d2134ef242627d7
nbjjans-pixel <1056796@hr.nl>
2024-10-10 14:47:35 +0200

    Oefen opgaven! les 3

---
+++ resources/views/products.blade.php
@@ -0,0 +1,2 @@
+<h1>Product ID: {{ $id }}</h1>
+<p><a href="{{route('about-us')}}">Ga naar about us pagina</a><p/>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +18,15 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+//andere mogelijkheden naar get zijn: patch, delete, put, post, options, match, any
+
+Route::resource('products', ProductController::class);
+Route::get('/about-us', [AboutUsController::class, 'show'])->name('about-us');
+
+Route::get('products/{id}', function(int $id) {
+    return view('products', ['id' => $id]);
+}) ->name('products');
+
 
 require __DIR__.'/auth.php';
