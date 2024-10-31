b15331f6b4f115d57c0560444092bdd48115bb49
nbjjans-pixel <1056796@hr.nl>
2024-10-31 15:56:45 +0100

    Tailwind reset, php storm en github bug test.

--- _changelog/README.md
+++ _changelog/README.md
@@ -1,4 +1,10 @@
 # Changelog
+
+## 31-10-2024
+- Geprobeerd te werken aan de diepere validatie
+- Grote vervelende bug waardoor php storm elke keer mij uit mijn bestanden gooit en weer terug zet naar de grootste map in mijn project
+- github wilt ook niet meer updaten
+- Docent om hulp gevraagd bij dit probleem zeker omdat het mij moeite gaat brengen met het presenteren van het project laat staan het afmaken.
 ## 28-10-2024
 Actie:
 
--- routes/web.php
+++ routes/web.php
@@ -27,29 +27,30 @@ Route::resource('products', ProductController::class);
 
 Route::get('/about-us', [AboutUsController::class, 'show'])->name('about-us');
 
-Route::get('products/{id}', function(int $id) {
+Route::get('products/{id}', function (int $id) {
     return view('products', ['id' => $id]);
 })->name('products');
 
 Route::get('/houses', [HouseController::class, 'listTitles'])->name('houses.list');
+
 Route::get('/houses/create', [HouseController::class, 'create'])->name('house.create');
 Route::post('/houses', [HouseController::class, 'store'])->name('house.store');
+
 Route::get('/houses/{id}', [HouseController::class, 'show'])->name('houses.show');
 Route::delete('/houses/{id}', [HouseController::class, 'destroy'])->name('houses.destroy');
 Route::get('/houses/{id}/edit', [HouseController::class, 'edit'])->name('houses.edit');
 Route::put('/houses/{id}', [HouseController::class, 'update'])->name('house.update');
+
 Route::get('/houses/{house}/review/create', [ReviewController::class, 'create'])->name('reviews.create');
 Route::post('/houses/{house}/reviews', [ReviewController::class, 'store'])->name('reviews.store');
 Route::post('/houses/{house}/favorite', [HouseController::class, 'favorite'])->name('houses.favorite');
 Route::delete('/houses/{house}/unfavorite', [HouseController::class, 'unfavorite'])->name('houses.unfavorite');
 
-
-//addmin
+// Admin
 Route::get('/admin/houses', [HouseController::class, 'adminIndex'])->name('admin.houses.index');
 Route::get('/admin/houses/{id}/edit', [HouseController::class, 'edit'])->name('admin.houses.edit');
 Route::put('/admin/houses/{id}', [HouseController::class, 'update'])->name('admin.houses.update');
 Route::delete('/admin/houses/{id}', [HouseController::class, 'destroy'])->name('admin.houses.destroy');
-
 
 Route::get('secret', [SecretController::class, 'show']);
 
